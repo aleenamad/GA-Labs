f5837844df58039c7371b0ecd67a4ff0fcb53d7b
AleenaMad <aleena.madni@gmail.com>
2017-12-06 12:01:10 -0600

    initial commit

---
+++ Day 3/conditional_practice.js
@@ -0,0 +1,14 @@
+//const isSunny = true;
+//const isRainy = false;
+
+
+//console.log(isSunny);
+//console.log(isRainy);
+
+//console.log(isRainy===isSunny);
+
+
+let toggle = true;
+console.log("this is toggle " + toggle);
+toggle = !toggle;
+console.log("this is toggle " + toggle);
