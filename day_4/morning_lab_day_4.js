f47685329766ba637a038e8afea5cb5144929346
AleenaMad <aleena.madni@gmail.com>
2017-12-07 12:11:12 -0600

    finished caluclate

--- day_4/morning_lab_day_4.js
+++ day_4/morning_lab_day_4.js
@@ -1,3 +1,4 @@
+// print greeting
 const printGreeting = (greet) => {
   console.log("Yo wasssssup" + " " + greet);
 }
@@ -5,7 +6,7 @@ const printGreeting = (greet) => {
 printGreeting("homie");
 printGreeting("essay");
 
-
+// reverse word order
 const reverseWordOrder = (word) => {
   var splitString = word.split("");
   var reverseArray = splitString.reverse();
@@ -14,3 +15,15 @@ const reverseWordOrder = (word) => {
 }
 
 console.log(reverseWordOrder("hey my name is"));
+
+// calculate
+const calculate = (num1, num2, operation) => {
+  if (operation === "sum") return num1 + num2;
+  if (operation === "sub") return num1 - num2;
+  if (operation === "mult") return num1 * num2;
+  if (operation === "div") return num1 / num2;
+  if (operation === "exp") return num1 ** num2;
+}
+
+console.log(calculate(4, 3, "sub"));
+console.log(calculate(4, 3, "exp"));
